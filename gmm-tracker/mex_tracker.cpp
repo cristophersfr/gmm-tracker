eae4a39f9c18b4062bbe2a08df8c0d11de27dc20
Cristopher <cristopherpk@gmail.com>
2016-05-19 17:39:21 -0700

    adding video sample.

---
+++ gmm-tracker/mex_tracker.cpp
@@ -0,0 +1,10 @@
+//
+//  mex_tracker.cpp
+//  gmm-tracker
+//
+//  Created by Cristopher Freitas on 4/17/16.
+//  Copyright © 2016 Cristopher Freitas. All rights reserved.
+//
+
+#include <stdio.h>
+
